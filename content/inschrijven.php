c8a990b9501b63b2277768f8fc49ee8345394006
Ronald Teune <ronald@teune.email>
2018-06-22 12:32:34 +0200

    Inschrijven 2

--- content/inschrijven.php
+++ content/inschrijven.php
@@ -11,6 +11,14 @@ $template = file_get_contents('includes/inschrijfdummy.tmpl'); // Generated from
 list($header, $footer) = explode('<p>PLEKJEVASTHOUDER</p>', $template);
 echo $header;
 
+if(!isFormSubmitted()) {
+    ?>
+    <p>Leuk dat je je wilt inschrijven! We vinden het fijn als je je zo vroeg mogelijk inschrijft, zodat we weten op
+        hoeveel mensen we moeten rekenen (qua eten etc.). Je krijgt na je inschrijving een mailtje, zodat je geld kan
+        overmaken. Nadat je het geld overmaakt is je inschrijving definitief.</p>
+    <?php
+}
+
 const ARBITRARY_CONSTANT_HIGH_ENOUGH_TO_ENSURE_PROPER_INPUT = 3;
 
 require_once('vendor/autoload.php');
@@ -43,14 +51,14 @@ $nogMeer->addField(new InsField('Wat zoek je of verwacht je op het KloosterBoerd
 $nogMeer->addField(new InsField('Waar denk je aan als je aan een boerderij denkt?', 'denkaanboerderij', false, $defaultSize));
 $nogMeer->addField(new InsField('Wat is je favoriete eten?', 'favorieteeten', false, $defaultSize));
 $nogMeer->addField(new InsField('Waar kom je vandaan?', 'waarvandaan', false, $defaultSize));
-$nogMeer->addField(new InsField('Heb je verder nog vragen?', 'verderevragen', false, $defaultSize));
+$nogMeer->addField(new InsAreaField('Heb je verder nog vragen of opmerkingen?', 'verderevragen', false, $defaultSize));
 $nogMeer->addField(new InsCaptchaField('Hoeveel is drie maal drie (mag ook als Pippi Langkous)', 'sommetje', false, $defaultSize));
 
 $form->addFieldset($nogMeer);
 
 $hasDuplicates = false;
 
-if (count($_POST) > ARBITRARY_CONSTANT_HIGH_ENOUGH_TO_ENSURE_PROPER_INPUT) {
+if (isFormSubmitted()) {
     $errors = $form->validate();
     if ($errors == "") {
         $name = $_POST['fname'] . ' ' . $_POST['lname'];
@@ -73,7 +81,7 @@ if (count($_POST) > ARBITRARY_CONSTANT_HIGH_ENOUGH_TO_ENSURE_PROPER_INPUT) {
 
                 Als je binnen <b>10 minuten</b> nog geen bevestiging hebt, laat het dan even weten via het e-mailadres hieronder: dan hebben wij namelijk waarschijnlijk niet je juiste adres.<br /><br />
                 Je inschrijving is definitief als het geld is overgemaakt.<br />
-                Ons rekeningnummer is: NL69 TRIO 0390 9403 21, t.n.v. R. Teune.<br /><br />
+                Ons rekeningnummer is: NL69 TRIO 0390 9403 21, t.n.v. G. Gelderblom.<br /><br />
 
                 Je hebt ingevuld:<br />
                 <?php
@@ -83,6 +91,8 @@ if (count($_POST) > ARBITRARY_CONSTANT_HIGH_ENOUGH_TO_ENSURE_PROPER_INPUT) {
                 $template = file_get_contents('includes/mail.txt');
                 $mailer = new Mailer('Kloosterboerderijfestival', 'info@kloosterboerderijfestival.nl', $email, $name, 'Je inschrijving voor het kloosterboerderijfestival 2018', $template, $message);
                 $mailer->send();
+                $mailer2 = new Mailer($name, $email, 'info@kloosterboerderijfestival.nl', 'Kloosterboerderijfestival', 'Mijn inschrijving voor het kloosterboerderijfestival 2018', $template, $message);
+                $mailer2->send();
             }
         } catch (Exception $e) {
             echo '<span style="color: red; "><b>Er is iets mis gegaan met je inschrijving! Something went wrong!</b></span>';
@@ -98,5 +108,11 @@ if (count($_POST) > ARBITRARY_CONSTANT_HIGH_ENOUGH_TO_ENSURE_PROPER_INPUT) {
 } else {
     $form->display();
 }
+
+function isFormSubmitted()
+{
+    return count($_POST) > ARBITRARY_CONSTANT_HIGH_ENOUGH_TO_ENSURE_PROPER_INPUT;
+}
+
 echo $footer;
 ?>
